2dcb90b5bf4e56576b9889c664f4feb1534c4a4e
Alexunder Pakhomchik <korsar7772@gmail.com>
2022-08-11 17:33:44 +0300

    add comment's in method; deleted type str in 12 line; remaked method _get_average_mark; remaked method get_scholarship; remaked class Aspirant

--- task_2.py
+++ task_2.py
@@ -1,56 +1,32 @@
+from functools import reduce
 class Student():
     def __init__(self, first_name: str, last_name: str, group: str, marks: list):
         self.first_name = first_name
         self.last_name = last_name
         self.group = group
         self.marks = marks
-    """def add_mark() Принимает оценку и добавляет её в marks.
-    Если значение меньше 0 или больше 10, 
-    то значение не добавляется"""
     def add_mark(self, mark):
+        """def add_mark() Принимает оценку и добавляет её в marks.
+        Если значение меньше 0 или больше 10,
+        то значение не добавляется"""
         if mark >= 0 and mark <= 10:
-            self.marks.append(str(mark))
-    """
-    def _get_average_mark()Возвращает средний балл"""
+            self.marks.append(mark)
     def _get_average_mark(self):
-        counter = 0
-        sum = 0
-        for i in self.marks:
-            sum += int(i)
-            counter += 1
-        return sum / counter
-        # print(sum / counter)
-    """
-    def get_scholarship() Возвращает сумму стипендии. 
-    Если средняя оценка студента равна 5, то сумма 500, иначе 150"""
+        """
+        def _get_average_mark()Возвращает средний балл"""
+        return reduce(lambda x, y: x + y, self.marks) / len(self.marks)
+
     def get_scholarship(self):
-        scholarship_low = f'Стипендия студента {self.first_name} {self.last_name} - 150'
-        scholarship_high = f'Стипендия студента {self.first_name} {self.last_name} - 500'
-        if self._get_average_mark() >= 5:
-            print(scholarship_high)
-        else:
-            print(scholarship_low)
+        """
+        def get_scholarship() Возвращает сумму стипендии.
+        Если средняя оценка студента равна 5, то сумма 500, иначе 150"""
+        print(f'Стипендия студента {self.first_name} {self.last_name} - {150 if self._get_average_mark() < 5 else 500}')
+
+
+
 class Aspirant(Student):
     def __init__(self, first_name: str, last_name: str, group: str, scientific_publications: list, marks: list):
-        self.first_name = first_name
-        self.last_name = last_name
-        self.group = group
-        self.marks = marks
+        super().__init__(first_name, last_name, group, marks)
         self.scientific_publications = scientific_publications
     def get_scholarship(self):
-        scholarship_low = f'Стипендия аспиранта {self.first_name} {self.last_name} - 250'
-        scholarship_high = f'Стипендия аспиранта {self.first_name} {self.last_name} - 700'
-        if self._get_average_mark() >= 5:
-            print(scholarship_high)
-        else:
-            print(scholarship_low)
-
-alex = Student('Alex', 'Pakhomchik', '221b', [])
-alex.add_mark(5)
-alex.add_mark(6)
-alex.get_scholarship()
-
-john = Aspirant('John', 'Wick', 'Continental', ['How to avenge a dog for 4 movies'], [])
-john.add_mark(7)
-john.add_mark(8)
-john.get_scholarship()
+        print(f'Стипендия аспиранта {self.first_name} {self.last_name} - {250 if self._get_average_mark() < 5 else 700}')
