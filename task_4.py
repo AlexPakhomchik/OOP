b0f93a9304e7f42bed3d286ea0d55ffdd7fecbe9
Alexunder Pakhomchik <korsar7772@gmail.com>
2022-08-13 09:51:14 +0300

    add 4 method

---
+++ task_4.py
@@ -0,0 +1,16 @@
+class Kg:
+    @staticmethod
+    def ton(ton: float):
+        print(f'In {ton} tons - {ton * 1000:.2f} kg')
+
+    @staticmethod
+    def centner(centner: float):
+        print(f'In {centner} centners - {centner * 100:.2f} kg')
+
+    @staticmethod
+    def pound(pound: float):
+        print(f'In {pound} pounds - {pound * 0.4535923745:.2f} kg')
+
+    @staticmethod
+    def pood(pood: float):
+        print(f'In {pood} poods - {pood * 16, 3804964:.2f} kg')
