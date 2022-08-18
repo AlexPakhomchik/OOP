43a31dd5378d1586b798d9492f788df4f1296dd7
Alexunder Pakhomchik <korsar7772@gmail.com>
2022-08-18 11:07:49 +0300

    repair class Nobody

--- task_1.py
+++ task_1.py
@@ -1,3 +1,5 @@
 class Nobody:
+    """Класс Nobody: принимает создает оъект с атрибутом 'name', который сохраняет в значение
+    'Nobody' либо переданное имя со строкой I'm Nobody, not {name}"""
     def __init__(self, name: str):
         self.name = f"I'm Nobody, not {name}" if self.name != 'Nobody' else 'Nobody'
